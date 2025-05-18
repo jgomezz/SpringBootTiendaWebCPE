0cb5fc6a0ec3153f110a468d1e0447d54cc19861
jgomezz <jgomezz@gmail.com>
2025-05-18 13:16:20 -0500

    create producto

--- src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -4,13 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pe.edu.tecsup.tienda.dtos.CategoriaDTO;
 import pe.edu.tecsup.tienda.dtos.ProductoDTO;
 import pe.edu.tecsup.tienda.entities.Producto;
 import pe.edu.tecsup.tienda.services.CategoriaService;
 import pe.edu.tecsup.tienda.services.ProductoService;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -40,6 +47,48 @@ public class ProductoController {
 
     }
 
+    @GetMapping("/create")
+    public String create(Model model) throws Exception {
+        log.info("call create()");
+
+        List<CategoriaDTO> categorias = categoriaService.findAll();
+        model.addAttribute("categorias", categorias);
+
+        Producto producto = new Producto();
+        model.addAttribute("producto", producto);
+
+        return "productos/create";
+    }
+
+
+    @PostMapping("/store")
+    public String store(@ModelAttribute("producto") ProductoDTO producto, Errors errors,
+                        @RequestParam("file") MultipartFile file,
+                        RedirectAttributes redirectAttrs) throws Exception{
+        log.info("call store(producto: " + producto + ")");
+
+        /*
+        if(file != null && !file.isEmpty()) {
+            String filename = System.currentTimeMillis() + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+            producto.setImagen_nombre(filename);
+            if(Files.notExists(Paths.get(STORAGEPATH))){
+                Files.createDirectories(Paths.get(STORAGEPATH));
+            }
+            Files.copy(file.getInputStream(), Paths.get(STORAGEPATH).resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+        }*/
+
+        producto.setCreado(LocalDateTime.now());
+        producto.setEstado(1);
+
+        // FIX : Categoria fija
+        producto.setCategoria(categoriaService.findById(1L).get());
+
+        productoService.save(producto);
+
+        redirectAttrs.addFlashAttribute("message", "Registro guardado correctamente");
+
+        return "redirect:/productos";
+    }
 
 
 }
