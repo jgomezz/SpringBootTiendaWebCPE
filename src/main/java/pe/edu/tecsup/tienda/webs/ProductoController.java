a22f7d78ee59993160c34dd15bddd117d4cca820
jgomezz <jgomezz@gmail.com>
2025-05-18 12:02:12 -0500

    create ProductoController

--- src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pe.edu.tecsup.tienda.dtos.ProductoDTO;
 import pe.edu.tecsup.tienda.entities.Producto;
 import pe.edu.tecsup.tienda.services.CategoriaService;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @Slf4j
 @Controller
+@RequestMapping("/productos")
 public class ProductoController {
 
     @Autowired
